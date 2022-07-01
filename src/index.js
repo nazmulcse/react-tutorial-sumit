2145f4437a13ac31a7288638c64ea12e9e58888d
Nazmul Hasan <nazmul.hasan@technovista.com.bd>
2022-07-01 22:31:30 +0600

    Lesson 7

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const index = 0;
-function Clock({local}) {
-  return (
-    <h1 className="heading" tabIndex={index}>
-      <span className="text">Hello {new Date().toLocaleDateString(local)}</span>
-    </h1>
-  );
-}
-ReactDOM.render(<Clock local="bn-BD"/>, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
