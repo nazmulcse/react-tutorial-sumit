043719e788d8aef1901d2aabf303b0d4598a3754
Nazmul Hasan <nazmul.hasan@technovista.com.bd>
2022-07-05 12:13:40 +0600

    Lesson-10 completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,7 @@ export default class Form extends React.Component {
         title: 'Javascript',
         description: 'Javascript is Scripting language',
         library: 'React',
+        isAwesome: true,
     };
 
     handleChange = (e) => {
@@ -20,14 +21,24 @@ export default class Form extends React.Component {
             this.setState({
                 library: e.target.value,
             });
+        } else if (e.target.type === 'checkbox') {
+            this.setState({
+                isAwesome: e.target.checked,
+            });
         }
     };
 
+    submitHandler = (e) => {
+        e.preventDefault();
+        const { title, description, library, isAwesome } = this.state;
+        console.log(title, description, library, isAwesome);
+    };
+
     render() {
-        const { title, description, library } = this.state;
+        const { title, description, library, isAwesome } = this.state;
         return (
             <div>
-                <form action="">
+                <form onSubmit={this.submitHandler}>
                     <input
                         type="text"
                         placeholder="Enter Title"
@@ -43,6 +54,12 @@ export default class Form extends React.Component {
                         <option value="React">React</option>
                         <option value="Angular">Angular</option>
                     </select>
+                    <br />
+                    <br />
+                    <input type="checkbox" checked={isAwesome} onChange={this.handleChange} />
+                    <br />
+                    <br />
+                    <input type="submit" value="Submit" />
                 </form>
             </div>
         );
